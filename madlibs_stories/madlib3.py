453692138e71463b18f7315a7c9eabf201576c4c
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2024-04-19 13:35:12 +0100

    Create a folder containing mad lib stories

---
+++ madlibs_stories/madlib3.py
@@ -0,0 +1,16 @@
+# Required user's inputs
+noun1 = input("Noun: ")
+noun2 = input("Another noun: ")
+noun_pl = input("Plural noun: ")
+adj1 = input("Adjective: ")
+adj2 = input("Another adjective: ")
+adv = input("Adverb: ")
+verb = input("Verb: ")
+
+# Mad lib number 3
+madlib = f"Space Adventure \nOnce upon a time, in a galaxy far, far away, \
+there was an adventurous {noun1} named Anakin. One day, Anakin hopped \
+aboard their {adj1} spaceship - his mission was to {verb} \
+a new {noun2}. As the spaceship soared through the {adj2} galaxy, \
+Anakin marvelled at the twinkling {noun_pl} he passed. {adv}, \
+an asteroid appeared out of nowhere and shot across their path."
