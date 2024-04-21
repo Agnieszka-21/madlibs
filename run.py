cd5bc75ebf0b2ca025a889ddedd40fad844c6706
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2024-04-21 18:51:29 +0100

    Add the Merriam-Webster dictionary API and check a word's grammatical function

--- run.py
+++ run.py
@@ -1,9 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+# Import all available stories for mad libs
 from madlibs_stories import madlib1, madlib2, madlib3, madlib4, madlib5, madlib6, madlib7, madlib8
+
+# Python library to choose a random story from the provided ones
 import random 
+
+
+# Two Python libraries used to access dictionary API & the file with the api key
+import json
+import requests
+import dictionary_api
+# Access the dictionary API key
+app_key = dictionary_api.API_KEY_SERVICE
+# A variable to get the user's inputs (words) for the stories, one by one
+users_word = "cat"
+# Code figured out based on the following tutorial: https://www.youtube.com/watch?v=hpc5jyVpUpw
+# Aiming to access 'fl', functional label of the given word (e.g. adjective, verb, etc.)
+response = requests.get(f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{users_word}?key={app_key}")
+word_checked = response.json()
+print(word_checked[0]['fl'])
 
 
 # Welcome screen
@@ -31,4 +47,4 @@ elif play_again_question == "N":
     end_game = "Okay, thanks for playing!"
     print(end_game)    
 else:
-    input("Type Y if you'd like to pla again, or N to finish: ")
+    input("Type Y if you'd like to play again or N to finish your game, then press Enter: ")
