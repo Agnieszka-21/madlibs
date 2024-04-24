a141b1d097366822a686687519ca2e9c30636707
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2024-04-24 11:40:26 +0100

    Create functions (welcome, word_inputs, look_up_word, validate_word) to clean up code

--- run.py
+++ run.py
@@ -1,56 +1,79 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-# Import all available stories for mad libs
-#from madlibs_stories import madlib1, madlib2, madlib3, madlib4, madlib5, madlib6, madlib7, madlib8
 
 # Python library to choose a random story from the provided ones
 import random 
-
 
 # Two Python libraries used to access dictionary API & the file with the api key
 import json
 import requests
 import dictionary_api
-# Access the dictionary API key
-app_key = dictionary_api.API_KEY_SERVICE
-# A variable to get the user's inputs (words) for the stories, one by one
-users_word = "cat"
-# Code figured out based on the following tutorial: https://www.youtube.com/watch?v=hpc5jyVpUpw
-# Aiming to access 'fl', functional label of the given word (e.g. adjective, verb, etc.)
-response = requests.get(f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{users_word}?key={app_key}")
-word_checked = response.json()
-print(word_checked)
-print(word_checked[0]['fl'])
 
 
 # Welcome screen
-welcome = "Welcome to MAD LIBS! \n \nHow to play: \nYou will be asked to provide certain words \
+def welcome():
+    welcome = "Welcome to MAD LIBS! \n \nHow to play: \nYou will be asked to provide certain words \
 (a noun, an adjective etc.) that are then inserted into a randomly selected story. \
 To provide your words, simply type them as prompted and confirm the submission of \
 each word by pressing Enter. Afterwards, simply read the complete story. Have fun!\n"
-print(welcome)
+    print(welcome)
+welcome()
 
 
-#Mad lib stories as a class - trying something
-# Required user's inputs
-noun1 = input("Noun: ")
-noun2 = input("Another noun: ")
-noun_pl = input("Plural noun: ")
-adj1 = input("Adjective: ")
-adj2 = input("Another adjective: ")
-adv = input("Adverb: ")
-verb = input("Verb: ")
+# Required user's inputs - words to fill any blanks in a mad lib
+def word_inputs():
+    global users_word
+    global noun1
+    noun1 = input("Noun: ").upper()
+    users_word = noun1
+    #noun2 = input("Another noun: ").upper()
+    #noun_pl = input("Plural noun: ").upper()
+    #adj1 = input("Adjective: ").upper()
+    #adj2 = input("Another adjective: ").upper()
+    #adv = input("Adverb: ").upper()
+    #verb = input("Verb: ").upper()
+word_inputs()
 
 
-#Class
+# Access the dictionary API key
+def look_up_word():
+    app_key = dictionary_api.API_KEY_SERVICE
+    # A variable to get the user's inputs (words) for the stories, one by one
+    #users_word = noun1 or noun2 or noun_pl or adj1 or adj2 or adv or verb
+    # Code figured out based on the following tutorial: https://www.youtube.com/watch?v=hpc5jyVpUpw
+    # Aiming to access 'fl', functional label of the given word (e.g. adjective, verb, etc.)
+    response = requests.get(f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{users_word}?key={app_key}")
+    global word_checked
+    word_checked = response.json()
+    print(word_checked)
+    #word_checked[0]['fl']
+    if 'fl' in word_checked: # needs work: check if there's a dictionary within the list that is word_checked
+        print(word_checked[0]['fl'])
+look_up_word()
+
+
+# Validate input - check if the provided word was found in the dictionary and if it is a correct type (adj, adv etc)
+def validate_word():
+    if word_checked[0]['fl'] == "noun":
+        print("Validation successful. You word is a noun.")
+    #except TypeError:
+        #input("It looks like your word has a typo. Try again: ")
+    else: 
+        print("It looks like something is wrong.")
+        input("Please try again - enter a noun here: ")
+validate_word()
+
+
+""" Trying something
+
+#Class Story for all available mad libs
 class Story:
     """
-    A mad lib story with blanks
-    """
+    #A mad lib story with blanks
+"""
     def __init__(self, title, text):
         self.title = title
         self.text = text
-
 
 madlib1 = Story("\nStrange Science", f"\nScience is full of {adv} strange facts and stories. Did you know that \
 rats can laugh when they are being tickled? Another fun {noun1} about rats is that \
@@ -111,7 +134,7 @@ have been to see dinosaurs {verb} through cities \
 and fly in the sky…")
 
 
-#print(madlib1.title, madlib1.story)
+# Lists of titles and texts for all available stories
 all_titles = [madlib1.title, madlib2.title, madlib3.title, madlib4.title, madlib5.title, madlib6.title, madlib7.title, madlib8.title]
 all_texts = [madlib1.text, madlib2.text, madlib3.text, madlib4.text, madlib5.text, madlib6.text, madlib7.text, madlib8.text]
 
@@ -127,7 +150,7 @@ def choose_story_randomly():
         title_indices = [all_titles.index(title)]
     # Choose a random title index
     randomly_chosen_title = random.choice(title_indices)
-    # Choose a text that matches the title
+    # Choose a text that matches the title (same index)
     matching_text = all_texts[randomly_chosen_title]
     # Print the full story (randomly chosen title with its text)
     print(all_titles[randomly_chosen_title])
@@ -141,8 +164,6 @@ choose_story_randomly()
 play_again_question = input("\nWould you like to play again (Y/N)? ").upper()
 
 if play_again_question == "Y":
-    #madlib_story = random.choice([madlib1, madlib2, madlib3, madlib4, madlib5, madlib6, madlib7, madlib8])
-    #madlib_story.madlib()
     new_game_how = input("If you would like to re-use your words with a different story, type A and press Enter. \
 If you'd like to start a new game, type B and press Enter: ").upper()
     if new_game_how == "A":
@@ -151,10 +172,12 @@ If you'd rather use a random story, type B and press Enter: ").upper()
         if random_or_not == "A":
             print(all_titles)
         elif random_or_not == "B":
-            choose_story_randomly() # needs to be adjusted so that the list of possible stories excludes the previously used story
+            choose_story_randomly() #needs to be adjusted so that the list of possible stories excludes the previously used story
 
 elif play_again_question == "N":
     end_game = "Okay, thanks for playing!"
     print(end_game)    
 else:
     input("Type Y if you'd like to play again or N to finish your game, then press Enter: ")
+
+"""
